83be5af970f78fbd006df2233b08432944aa3ad3
am021975 <ax655239652229@gmail.com>
2015-04-21 23:16:18 +0800

    am021975

---
+++ am021975.java
@@ -0,0 +1,15 @@
+
+
+public class am021975
+{
+	public static void main(String[] args)
+	{
+		a b = new a();
+		System.out.println(b.b(10,20));
+	}
+}
+class a {
+	public int b(int a,int b){
+		return a*b;
+	}
+}
